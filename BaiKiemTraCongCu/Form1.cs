a12b65b3b1cd01cf63ff2bab6bb7b76ffa3305c5
hokhanhdu <khanhduatd1862002@gmail.com>
2024-03-15 13:58:29 +0700

    code chức đăng nhập

--- BaiKiemTraCongCu/Form1.cs
+++ BaiKiemTraCongCu/Form1.cs
@@ -16,5 +16,27 @@ namespace BaiKiemTraCongCu
         {
             InitializeComponent();
         }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUserName.Text; // Lấy tên người dùng từ textbox
+            string password = txtpassword.Text; // Lấy mật khẩu từ textbox
+
+            // Kiểm tra nếu username và password đúng
+            if (username == "admin" && password == "admin")
+            {
+                // Nếu đúng, hiển thị thông báo đăng nhập thành công và chuyển qua trang mới
+                MessageBox.Show("Đăng nhập thành công!");
+                Form2 form2 = new Form2();
+                // Hiển thị Form2
+                form2.Show();
+                this.Hide();
+            }
+            else
+            {
+                // Nếu sai, hiển thị thông báo lỗi
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng!");
+            }
+        }
     }
 }
